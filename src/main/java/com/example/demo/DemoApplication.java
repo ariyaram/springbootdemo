20739798132d3b0f4a34ad858c371bf42600ca79
RamaRao Ariyaram <ramarao.ariyaram@accenture.com>
2018-05-09 14:54:35 +0530

    exception api added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,32 +5,40 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @EnableAutoConfiguration
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/success")
-    String home() {
-        return "Hello World!";
+    @RequestMapping(value="/success", method = RequestMethod.GET)
+    public ResponseEntity<String>   testSuccess() {
+		System.out.println("testSuccess ***************:");
+       return new ResponseEntity<>("Hello World!", HttpStatus.OK);
     }
-	
-	 @RequestMapping("/error")
-    String home1() {
+
+	 @RequestMapping(value="/hi", method = RequestMethod.GET)
+    public ResponseEntity<String>  testError() {
 	    int i=0;
-	    
+System.out.println("testError *********************:");
 	    try{
-		    
+
 	    if(i==0){
-		    throw new Exception();
+			System.out.println("22222222222222222222222:");
+		    throw new Exception("exception raised");
 	    }
 	    } catch(Exception e){
+			System.out.println("Exception *****************:");
 			    e.printStackTrace();
 		    }
-        return "Hello World!";
+
+		     return new ResponseEntity<>("Hello World! Error", HttpStatus.OK);
+
     }
-    
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
