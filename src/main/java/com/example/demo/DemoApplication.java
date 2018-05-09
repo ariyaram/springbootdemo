b2cd38c0df429fb8c137bce9a412d206ba3d23ab
ariyaram <ramu.ariyaram@gmail.com>
2018-05-09 13:37:11 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
+    @RequestMapping("/success")
     String home() {
         return "Hello World!";
     }
